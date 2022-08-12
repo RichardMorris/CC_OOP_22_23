9e2f2a32482c33b525428700042d8fcf7b41052c
Richard Morris <rich@singsurf.org>
2022-08-12 10:44:34 +0100

    Better practice for stdin method

--- src/uk/ac/cornwallcollege/richardmorris/java2223/Hello.java
+++ src/uk/ac/cornwallcollege/richardmorris/java2223/Hello.java
@@ -2,17 +2,21 @@ package uk.ac.cornwallcollege.richardmorris.java2223;
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
+import java.io.IOException;
 
 public class Hello {
-    BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
 
     public void word() {
         System.out.print("word");
     }
     
-    public void great() throws Exception {
-        String name = in.readLine();
-        System.out.println("Hello "+name);
+    public void great() {
+        try(BufferedReader in = new BufferedReader(new InputStreamReader(System.in))) {
+            String name = in.readLine();
+            System.out.println("Hello "+name);
+        } catch(IOException ex) {
+            System.out.println(ex);
+        }
     }
 
     public static void main(String[] args) {
