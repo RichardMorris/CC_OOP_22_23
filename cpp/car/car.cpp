3d801f407d0e8a0ff672bb506b60e426bcbfd5b5
Richard Morris <richard.morris@cornwall.ac.uk>
2023-04-27 12:51:16 +0100

    make car actually compile

--- cpp/car/car.cpp
+++ cpp/car/car.cpp
@@ -7,15 +7,53 @@ class drivable {
         virtual void drive() = 0; // no implementation
 };
 
+class tire
+{
+private:
+    /* data */
+public:
+    tire    (/* args */);
+    ~tire    ();
+};
+
+tire::tire(/* args */)
+{
+}
+
+tire::~tire()
+{
+}
+
+
+class wheel {
+    public:
+        // wheel() {
+        //     std::cout << "wheel default constructor" << std::endl;
+        // }
+
+        wheel(tire t) : t(t) {
+            std::cout << "wheel constructor" << std::endl;
+        }
+        ~wheel() {
+            std::cout << "wheel destructor" << std::endl;
+        }
+    
+        void chaange_tire(tire t1) {
+            t = t1;
+        }
+    private:
+        tire t; // this is an association, the lifetime of the tire is not managed by the wheel
+
+};
+
+
 // car implements drivable
 class car : public drivable {
     public:
-        car() {
+        car()  
+        : fl(tire()), fr(tire()), rl(tire()), rr(tire())
+        {
             std::cout << "car constructor" << std::endl;
-            fl = wheel(tire());
-            fr = wheel(tire());
-            rl = wheel(tire());
-            rr = wheel(tire());
         }
         ~car() {
             std::cout << "car destructor" << std::endl;
@@ -33,40 +71,8 @@ class car : public drivable {
 };
 
 
-class wheel {
-    public:
-        wheel() {
-            std::cout << "wheel constructor" << std::endl;
-        }
-
-        wheel(tire t) : t(t) {
-            std::cout << "wheel constructor" << std::endl;
-        }
-        ~wheel() {
-            std::cout << "wheel destructor" << std::endl;
-        }
-    
-        void chaange_tire(tire t1) {
-            t = t1;
-        }
-    private:
-        tire t; // this is an association, the lifetime of the tire is not managed by the wheel
-
-};
-
-class tire
-{
-private:
-    /* data */
-public:
-    tire    (/* args */);
-    ~tire    ();
-};
-
-tire::tire(/* args */)
-{
-}
-
-tire::~tire()
-{
+int main() {
+    car c;
+    c.drive();
+    return 0;
 }
